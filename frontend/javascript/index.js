0269b2d6916281945391cdedc0b33fe75fe5512e
Konnor Rogers <konnor5456@gmail.com>
2024-11-04 00:59:55 -0500

    make light-code look like syntax blocks and add tooltips (#6)

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -21,6 +21,7 @@ import "@shoelace-style/shoelace/dist/components/icon-button/icon-button.js"
 import "@shoelace-style/shoelace/dist/components/menu/menu.js"
 import "@shoelace-style/shoelace/dist/components/menu-item/menu-item.js"
 import "@shoelace-style/shoelace/dist/components/menu-label/menu-label.js"
+import "@shoelace-style/shoelace/dist/components/tooltip/tooltip.js"
 import "@shoelace-style/shoelace/dist/components/visually-hidden/visually-hidden.js"
 setBasePath("/shoelace-assets")
 
